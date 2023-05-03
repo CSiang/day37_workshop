a8f630e232d8238033c5acfaa43a0d1baf7ac7f7
CSiang <cscorpio87@gmail.com>
2023-05-03 14:42:21 +0800

    initial commit

---
+++ Server/src/main/java/csf2022/day37_workshop/Day37WorkshopApplication.java
@@ -0,0 +1,13 @@
+package csf2022.day37_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day37WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day37WorkshopApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/csf2022/day37_workshop/Day37WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package csf2022.day37_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day37WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
